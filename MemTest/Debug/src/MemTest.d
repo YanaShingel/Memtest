f2a71acb45abdd9412b3d2c76ee7d188d2f5ffd1
YanaShingel <yana.misyuchenko.94@mail.ru>
2015-12-20 22:11:39 +0400

    Алгоритмы тестирования памяти

---
+++ MemTest/Debug/src/MemTest.d
@@ -0,0 +1,1 @@
+src/MemTest.d: ../src/MemTest.c
---
+++ MemTest/src/sizes.h
@@ -0,0 +1,13 @@
+/*
+ * sizes.h
+ *
+ *  Created on: 20.12.2015
+ *      Author: student
+ */
+
+#ifndef SIZES_H_
+#define SIZES_H_
+
+
+
+#endif /* SIZES_H_ */
